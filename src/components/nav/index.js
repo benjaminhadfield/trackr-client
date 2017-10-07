067c34a59b905735d27178b6b01c00ebf36c4e4f
Ben Hadfield <benjohnhadfield@gmail.com>
2017-10-07 17:22:11 +0100

    remove crappy mobile safari test

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -4,18 +4,11 @@ import Charges from 'material-ui-icons/AttachMoney'
 import Add from 'material-ui-icons/Add'
 import Sent from 'material-ui-icons/PresentToAll'
 
-const mobile = /mobile/gi
-const safari = /safari/gi
-const isOnMobileSafari = mobile.test(navigator.userAgent) && safari.test(navigator.userAgent)
-
 const Nav = ({ selected, onChange }) => (
   <BottomNavigation
     showLabels
     value={selected}
     onChange={onChange}
-    style={{
-      paddingBottom: isOnMobileSafari ? '44px' : '0'
-    }}
   >
     <BottomNavigationButton label='Charges' icon={<Charges />} />
     <BottomNavigationButton label='Create New' icon={<Add />} />
